5cdbebd21f4ab087e8f9a002be44191aaca8cb86
Birger Moëll <birger.moell@gmail.com>
2019-04-09 17:39:14 +0200

    working sockets

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+var express = require('express')
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
@@ -5,6 +6,8 @@ var io = require('socket.io')(http)
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
+
+app.use('/public', express.static(__dirname + '/public'))
 
 io.on('connection', function (socket) {
   console.log('an user connected')
